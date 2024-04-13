3d5e6a7231a81e477a3f7ee2f8bee960db7c58ba
Leonardo Campos <leonardoworld@outlook.com>
2024-04-13 03:09:49 -0300

    Commit

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,11 +1,15 @@
 document.querySelectorAll("#link").forEach(function (data) {
   data.addEventListener("click", (event) => {
     const dataHref = data.getAttribute("data-href");
+    console.log(dataHref);
 
     switch (dataHref) {
+      /* HOME */
       case "clickup":
-        window.open("https://chat.openai.com/", "_blank");
+        window.open("https://app.clickup.com/", "_blank");
         break;
+
+      /* PROGRAMMING */
       case "quickref":
         window.open("https://quickref.me/", "_blank");
         break;
