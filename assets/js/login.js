7a2aebae9556e2b74ee1d9710cb01f4230b02288
Leonardo Campos <leonardoworld@outlook.com>
2024-04-14 13:26:09 -0300

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,7 +7,7 @@ function logar(event) {
     return;
   }
 
-  if (userLogin === "admin" && senhaLogin === "admin") {
+  if (userLogin === "quick" && senhaLogin === "quickpass") {
     location.href = "main.html"; // Redirecione para a página main.html
   } else {
     alert("Usuário ou senha incorreto!"); // Exiba uma mensagem de erro caso as credenciais estejam incorretas
