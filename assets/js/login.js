7211bb5ed83bd723b1b80fa1a62fdff62e42ef3b
Leonardo Campos <leonardoworld@outlook.com>
2024-04-14 13:24:28 -0300

    Commit

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,11 +2,14 @@ function logar(event) {
   let userLogin = document.getElementById("usuario").value;
   let senhaLogin = document.getElementById("senha").value;
 
+  if (userLogin === "" || senhaLogin === "") {
+    alert("Preencha todos os campos!"); // Exiba uma mensagem de erro caso os campos estejam vazios
+    return;
+  }
+
   if (userLogin === "admin" && senhaLogin === "admin") {
     location.href = "main.html"; // Redirecione para a página main.html
   } else {
     alert("Usuário ou senha incorreto!"); // Exiba uma mensagem de erro caso as credenciais estejam incorretas
   }
-
-  return false;
 }
