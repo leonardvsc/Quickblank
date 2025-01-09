a357d26a4b195fdd1a3adf787fd31bf231947e60
Leonardo Campos <120220554+leonardvsc@users.noreply.github.com>
2025-01-09 20:13:17 -0300

    Update Header.jsx

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   RiGithubLine,
   RiInstagramLine,
@@ -5,10 +7,20 @@ import {
 } from "@remixicon/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export function Header() {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <header className="bg-[#292929]">
+    <header
+      className={`fixed top-0 z-50 w-full duration-300 ${scrollY > 100 ? "bg-[#292929] shadow" : ""}`}
+    >
       <div className="mx-auto flex max-w-7xl items-center justify-between px-6 py-6 lg:px-8">
         <Link href={"/"}>
           <Image src={"/logo.svg"} alt="Logo" width={200} height={43} />
